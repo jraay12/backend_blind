f0b4f980047a6e5bb66f1206266cf24473ffd273
John Ray <johnraycaete@gmail.com>
2023-11-08 19:18:26 +0800

    changes

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("address_id")->constrained("address")->onUpdate("cascade")->onDelete("cascade");
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role');
+            $table->string('number');
             $table->rememberToken();
             $table->timestamps();
         });
