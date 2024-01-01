290356bba51eb7041598a7ef4e951a783b40fb41
John Ray <johnraycaete@gmail.com>
2024-01-01 11:12:37 +0800

    update fucntion

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -29,13 +29,14 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
         Route::get("/total-user", [UserController::class, "userDetails"]);
         Route::delete("/delete/{id}", [UserController::class, "destroy"]);
         Route::get("/user-contacts/{userId}", [UserController::class, "userContactsDetails"]);
+        Route::patch("/update-user/{id}", [UserController::class, "update"]);
+
      });
      Route::group(['middleware' => ['userPermission']], function(){
         Route::post("/add-contacts", [ContactsController::class, "contacts"]);
         Route::get("/contacts-details/{id}", [ContactsController::class, "contactDetails"]);
         Route::delete("/delete-contacts/{id}", [ContactsController::class, "destroy"]);
-        Route::patch("/update-user/{id}", [UserController::class, "update"]);
-        Route::patch("update-contacts/{id}", [ContactsController::class, "updateContacts"]);
+        Route::patch("/update-contacts/{id}", [ContactsController::class, "updateContacts"]);
 
 
     });
